364b5fbc6c8296d78723f5184ef85bf3fd56d87d
Anton Golub <mailbox@antongolub.ru>
2018-07-12 18:43:38 +0300

    perf: tech release

--- scripts/release-hooks/isLastPack.js
+++ scripts/release-hooks/isLastPack.js
@@ -12,15 +12,20 @@ let count = JSON.parse(fs.readFileSync(COUNT))
 
 console.log('release-hooks: current path is ' + __dirname)
 if (count > 0) {
-  if (count !== total) {
-    console.log('release-hooks: drop tag')
+  try {
+    if (count !== total) {
+      console.log('release-hooks: drop tag')
 
-    exec(`cd ../..`)
-    exec(`sh ${DROP_TAG}`)
-    exec(`cd ${__dirname}`)
+      exec(`cd ../..`)
+      exec(`sh ${DROP_TAG}`)
+      exec(`cd ${__dirname}`)
+    }
+    exec(`sh ${DECREMENT}`, {stdio:['inherit','pipe','pipe']})
+    count -= 1
+
+  } catch (err) {
+    console.log('release-hooks [error]: ', err)
   }
-  exec(`sh ${DECREMENT}`)
-  count -= 1
 }
 const isLastPack = count < 1
 

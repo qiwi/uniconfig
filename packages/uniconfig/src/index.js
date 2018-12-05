3598d55a7f76ebe841bafd0440afb589180ffda6
Anton Golub <mailbox@antongolub.ru>
2018-12-05 17:14:58 +0300

    feat: add path plugin to uniconfig pack

--- packages/uniconfig/src/index.js
+++ packages/uniconfig/src/index.js
@@ -2,10 +2,12 @@ import unicore, {rollupPlugin} from '@qiwi/uniconfig-core'
 import jsonPlugin from '@qiwi/uniconfig-plugin-json'
 import filePlugin from '@qiwi/uniconfig-plugin-api-file'
 import datatreePlugin from '@qiwi/uniconfig-plugin-datatree'
+import pathPlugin from '@qiwi/uniconfig-plugin-path'
 
 rollupPlugin(jsonPlugin)
 rollupPlugin(filePlugin)
 rollupPlugin(datatreePlugin)
+rollupPlugin(pathPlugin)
 
 export * from '@qiwi/uniconfig-core'
 export default unicore

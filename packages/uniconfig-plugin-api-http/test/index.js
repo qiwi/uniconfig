e111790cd0bedb9640d57bb3bc9df26dcabbc90c
Anton Golub <mailbox@antongolub.ru>
2019-05-10 15:08:52 +0300

    chore(package): up deps

--- packages/uniconfig-plugin-api-http/test/index.js
+++ packages/uniconfig-plugin-api-http/test/index.js
@@ -8,6 +8,7 @@ describe('uniconfig-plugin-api-http', () => {
   const expectedData = {
     data: {
       id: 2,
+      email: 'janet.weaver@reqres.in',
       first_name: 'Janet',
       last_name: 'Weaver',
       avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg'

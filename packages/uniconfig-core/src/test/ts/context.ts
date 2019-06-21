a2167f8f36747f48034853fef7ac9f8ef21ff714
Anton Golub <mailbox@antongolub.ru>
2019-06-21 16:23:03 +0300

    chore: sync jest v between packs, update configs

---
+++ packages/uniconfig-core/src/test/ts/context.ts
@@ -0,0 +1,13 @@
+import createContext, {Context} from '../../main/ts/context'
+import pipeRegistry from '../../main/ts/pipe/pipeRegistry'
+
+describe('context', () => {
+  describe('constructor', () => {
+    it('returns proper instance', () => {
+      const context = createContext()
+
+      expect(context).toBeInstanceOf(Context)
+      expect(context.pipe).toBe(pipeRegistry)
+    })
+  })
+})
